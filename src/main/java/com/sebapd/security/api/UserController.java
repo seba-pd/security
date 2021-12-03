0c89f7f1fc9cb6218a5df12e29ae46679b153796
seba-pd <56122757+seba-pd@users.noreply.github.com>
2021-12-03 12:24:36 +0100

    Update UserController.java

--- src/main/java/com/sebapd/security/api/UserController.java
+++ src/main/java/com/sebapd/security/api/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/")
+@RestController
 public class UserController {
 
     private final JpaUserDetailsService userDetailsService;
